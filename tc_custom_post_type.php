7f4615c0411e986ff80826d6f426274078c984a9
kzjlee <kzjlee@gmail.com>
2016-03-19 18:20:35 -0400

    file linking issue

--- tc_custom_post_type.php
+++ tc_custom_post_type.php
@@ -64,5 +64,7 @@ register_activation_hook( __FILE__, 'my_rewrite_flush' );
 
 // Link to shortcode file 
 
-include( plugin_dir_path( __FILE__ ) . 'tc_shortcode');
+include( plugin_dir_path( __FILE__ ) . 'tc_shortcode.php');
+
+// shortcode
 
