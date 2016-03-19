69e1125ef7527aa51c4e8c73caa94c1f93718dac
chungyulay <laichungyu@LaitekiMacBook-Pro.local>
2016-03-19 19:37:32 -0400

    Link the shortcode php to stylesheet
    Link the shortcode php to stylesheet

--- tc_shortcode.php
+++ tc_shortcode.php
@@ -1,5 +1,24 @@
 <?php
 
+// Link the shortcode php to stylesheet
+function my_plugin_enqueue_scripts() {
+	wp_enqueue_style('my-shortcode',plugins_url('my-plugin/css/style.css')); 
+}
+add_action( 'wp_enqueue_scripts', 'my_plugin_enqueue_scripts' );
+
+
+// Add Color to the font 
+
+function text_color( $atts , $content=null ) {
+	extract( shortcode_atts(
+		array('color'=>'black',
+			), $atts )
+	);
+	return '<div class="section '.$color.'">'.$content.'</div>';
+
+}
+add_shortcode( 'text_color', 'text_color' );
+
 
 //calling my custom postype function
 add_shortcode('tc_shortcode', function(){
