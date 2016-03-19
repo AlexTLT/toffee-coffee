918b886dec36560eb937ceaabbd16b70cb363ff5
kzjlee <kzjlee@gmail.com>
2016-03-19 15:42:55 -0400

    name

---
+++ toffee-coffee-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: toffee-coffee-plugin
+ * Plugin URI: https://phoenix.sheridanc.on.ca/~ccit3470/
+ * Description: Assignment 3: create a wordpress plugin
+ * Author: Alex Tsai, Chungyu Lay, Keizac Lee
+ * Author URI: 
+ * Version: 1.0.1
+ */
+
+?>
