3c377a1b02259d6a6b4e06d0e167f1ec22d49298
chungyulay <laichungyu@LaitekiMacBook-Pro.local>
2016-03-19 17:55:08 -0400

    edit tc Shortcode
    edit tc Shortcode

--- tc_shortcode.php
+++ tc_shortcode.php
@@ -1,18 +1,22 @@
 <?php
 
 
-//calling my custom postype function 
-add_action('init', 'coffeetoffee_custom_postypes');
+//calling my custom postype function
+add_shortcode('tc_custom_post_types', function(){
+  $loop = new WP_Query(
+    $array(
+      'post-type' => 'reviews',
+      'orderby' => 'title'
+      )
+  );
 
-function my_rewrite_flush() {
-    // First, we "add" the custom post type via the above written function.
-    // Note: "add" is written with quotes, as CPTs don't get added to the DB,
-    // They are only referenced in the post_type column with a post entry, 
-    // when you add a post of this CPT.
-    coffeetoffee_custom_postypes();
+  if ($loop-> have_post() ) {
+    $output = '<ul class="tc_custom_post_types">';
 
-    // ATTENTION: This is *only* done during plugin activation hook in this example!
-    // You should *NEVER EVER* do this on every page load!!
-    flush_rewrite_rules();
-}
-register_activation_hook( __FILE__, 'my_rewrite_flush' );
+      while ($loop-> have_post() ) {
+        $loop-> the_post();
+        $meta = get_post_meta(get_the_id());
+        print_r($meta);
+      }
+  }
+});
