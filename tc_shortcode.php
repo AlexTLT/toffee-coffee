73900dba15ce488659005c0b7321515cca590e0d
chungyulay <laichungyu@LaitekiMacBook-Pro.local>
2016-03-19 17:44:44 -0400

    tc Shortcode

---
+++ tc_shortcode.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//calling my custom postype function 
+add_action('init', 'coffeetoffee_custom_postypes');
+
+function my_rewrite_flush() {
+    // First, we "add" the custom post type via the above written function.
+    // Note: "add" is written with quotes, as CPTs don't get added to the DB,
+    // They are only referenced in the post_type column with a post entry, 
+    // when you add a post of this CPT.
+    coffeetoffee_custom_postypes();
+
+    // ATTENTION: This is *only* done during plugin activation hook in this example!
+    // You should *NEVER EVER* do this on every page load!!
+    flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'my_rewrite_flush' );
